3ced26abb6393238f5610ee000a21bd2ef1397de
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-04-01 00:28:15 +0800

    update

---
+++ client/src/components/AboutSection.tsx
@@ -0,0 +1,10 @@
+
+export default function AboutSection() {
+    return(
+        <>
+            <div className="flex flex-1 bg-red-100 h-10">
+
+            </div>
+        </>
+    );
+}
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,10 +1,25 @@
 import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
 
 export default function MainPage() {
     return(
         <>
-            <div className="flex flex-col max-w-[880px] my-10 mx-auto h-auto items-center">
+            <div className="flex flex-col max-w-[880px] my-10 mx-auto h-auto">
                <HeroSection />
+                
+                <div className="flex">
+                    <div>
+                        <AboutSection />
+                         <span className="bg-blue-100">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi aut qui sunt. Harum ducimus quidem quae temporibus iure distinctio magnam assumenda quo reiciendis cupiditate rerum, voluptatem excepturi commodi tempore tenetur.
+                        </span>
+                    </div>
+
+
+                    <span className="max-w-[280px]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi aut qui sunt. Harum ducimus quidem quae temporibus iure distinctio magnam assumenda quo reiciendis cupiditate rerum, voluptatem excepturi commodi tempore tenetur.
+                    </span>
+                </div>
             </div>
         </>
     );
