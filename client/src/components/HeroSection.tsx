870a770e8e20734a36f9368ed5b90a48037d8659
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-03-31 20:26:52 +0800

    Hero Section added

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -1,20 +1,55 @@
+import { useState } from "react";
 import MyProfile from "../assets/Hanni.jpeg"
+import { MapPin, Mail, FileText, Sun, Moon } from 'lucide-react';
+
 
 export default function HeroSection() {
+
+    const [dark, setDark] = useState(false);
+
+    const toggleTheme =  () => {
+        setDark(!dark);
+        document.documentElement.classList.toggle("dark");
+    };
+
     return(
         <>
-            <div className="flex w-full bg-red-100 gap-x-5">
+            <div className="flex w-full gap-x-5">
                 <img src={MyProfile} alt="My Profile" className="w-[160px] h-[160px] object-cover" />
                 
-                <div className="flex flex-col w-full pr-5">
+                <div className="flex flex-col w-full pr-5 gap-y-1">
 
                     <div className="flex items-center justify-between">
                         <h1 className="text-xl font-semibold">John Patrick Soriaga</h1>
 
-                        <span>Dark Mode Later</span>
+                        <button onClick={toggleTheme} className="bg-[#d1d5dc] cursor-pointer relative flex items-center w-15 h-7 p-0.5">
+                        <div className={`flex items-center justify-center top-1 left-1 w-7 h-6 bg-white transform transition ${dark ? "translate-x-7" : ""}`}>
+                            <Sun size={16} className=" z-10" />
+                            <Moon size={16} className="ml-auto text-white z-10 hidden" />
+                        </div>
+
+                        </button>
+
                     </div>
 
-                    
+                    <div>
+                        <MapPin className="inline-block mr-1" size={14} />
+                        <span className="text-sm">Bulacan, Philippines</span>
+                    </div>
+
+                    <h1 className="font-normal tracking-wide my-3">BSIT Student <span className="text-gray-400">\</span> Aspiring Web Developer</h1>
+
+                    <div className="flex gap-x-3">
+                        <button className="flex items-center justify-center gap-x-2 bg-black py-2 px-3 rounded-xs cursor-pointer">
+                            <FileText className="text-white" size={14} />
+                            <span className="text-sm text-white">View Resume</span>
+                        </button>
+
+                        <button className="flex items-center justify-center gap-x-2 py-2 px-3 rounded-xs shadow-sm cursor-pointer">
+                            <Mail size={14} />
+                            <span className="text-sm">Send Email</span>
+                        </button>
+                    </div>
 
                 </div>
             </div>
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import HeroSection from "../components/HeroSection";
 export default function MainPage() {
     return(
         <>
-            <div className="flex flex-col bg-black/10 max-w-[880px] my-10 mx-auto h-auto items-center">
+            <div className="flex flex-col max-w-[880px] my-10 mx-auto h-auto items-center">
                <HeroSection />
             </div>
         </>
